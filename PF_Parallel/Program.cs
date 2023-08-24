0e154002c63fd81347e4dbaa501fa014c54b111e
JMacasUK <139647338+JMacasUK@users.noreply.github.com>
2023-08-23 19:28:32 -0700

    Refactorizacion y mejoras visuales

--- PF_Parallel/Program.cs
+++ PF_Parallel/Program.cs
@@ -7,13 +7,16 @@ namespace PF_Parallel
 {
     internal class Program
     {
-        static string rutaEscanear = "C:\\DataPort\\sops\\Programs\\Windows";
+        static string rutaEscanear = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         static string[] archivos = Directory.GetFiles(rutaEscanear, "*.*", SearchOption.AllDirectories);
+        static TimeSpan espacioDeTiempoParallel;
+        static TimeSpan espacioDeTiempoSequential;
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"Se leyeron {archivos.Length} en el directorio {rutaEscanear}.");
-            
+            Console.WriteLine($"Se detectaron {archivos.Length} archivos en el directorio {rutaEscanear}");
+            Console.WriteLine("");
+
             ParallelForEach();
 
             SequentialForEach();
@@ -37,16 +40,16 @@ namespace PF_Parallel
                         fileStream.Position = 0;
                         byte[] hashValue = sha256.ComputeHash(fileStream);
 
-                        Console.Write($"{fileInfoArchivo.Name}: ");
-                        ImprimirByteArrayHexadecimal(hashValue);
+                        //Console.Write($"{fileInfoArchivo.Name}: ");
+                        //ImprimirByteArrayHexadecimal(hashValue);
                     }
-                    catch (IOException ioe)
+                    catch (IOException ioException)
                     {
-                        Console.WriteLine($"Excepción en E/S: {ioe.Message}");
+                        Console.WriteLine($"Excepción en E/S: {ioException.Message}");
                     }
-                    catch (UnauthorizedAccessException uae)
+                    catch (UnauthorizedAccessException uaException)
                     {
-                        Console.WriteLine($"Excepción de acceso: {uae.Message}");
+                        Console.WriteLine($"Excepción de acceso: {uaException.Message}");
                     }
                 }
             }
@@ -56,8 +59,7 @@ namespace PF_Parallel
         {
             for (int i = 0; i < byteArray.Length; i++)
             {
-                Console.Write($"{byteArray[i]:X2}");
-                if ((i % 4) == 3) Console.Write(" ");
+                Console.Write($"{byteArray[i]:X2}".ToLowerInvariant());
             }
 
             Console.WriteLine();
@@ -66,7 +68,7 @@ namespace PF_Parallel
 
         public static void ParallelForEach()
         {
-            Console.WriteLine($"Ejecutando ParallelForEach(), fecha y hora de inicio: {DateTime.Now.ToString().ToUpperInvariant()}");
+            Console.WriteLine($"Ejecutando ParallelForEach(), fecha y hora de inicio: {DateTime.Now:O}");
             
             Stopwatch cronometro = new Stopwatch();
             
@@ -74,16 +76,15 @@ namespace PF_Parallel
             Parallel.ForEach(archivos, CalcularSHA256);
             cronometro.Stop();
             
-            TimeSpan espacioDeTiempo = cronometro.Elapsed;
+            TimeSpan espacioDeTiempoParallel = cronometro.Elapsed;
             
-            string tiempoTranscurridoString = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
-                espacioDeTiempo.Hours, espacioDeTiempo.Minutes, espacioDeTiempo.Seconds, espacioDeTiempo.Milliseconds);
+            string tiempoTranscurridoString = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                espacioDeTiempoParallel.Hours, espacioDeTiempoSequential.Minutes, espacioDeTiempoSequential.Seconds, espacioDeTiempoSequential.Milliseconds);
             
+            Console.WriteLine($"Finalizado ParallelForEach(), fecha y hora de finalización: {DateTime.Now:O}");
             Console.WriteLine($"Tiempo transcurrido: {tiempoTranscurridoString}");
-            
-            Console.WriteLine($"Finalizado ParallelForEach(), fecha y hora de finalización: {DateTime.Now.ToString().ToUpperInvariant()}");
 
-            Console.WriteLine(Environment.NewLine);
+            Console.Write(Environment.NewLine);
         }
 
         public static void SequentialForEach()
@@ -101,16 +102,15 @@ namespace PF_Parallel
 
             cronometro.Stop();
 
-            TimeSpan espacioDeTiempo = cronometro.Elapsed;
+            TimeSpan espacioDeTiempoSequential = cronometro.Elapsed;
 
             string tiempoTranscurridoString = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
-                espacioDeTiempo.Hours, espacioDeTiempo.Minutes, espacioDeTiempo.Seconds, espacioDeTiempo.Milliseconds);
-
+                espacioDeTiempoSequential.Hours, espacioDeTiempoSequential.Minutes, espacioDeTiempoSequential.Seconds, espacioDeTiempoSequential.Milliseconds);
+            
+            Console.WriteLine($"Finalizado SequentialForEach(), fecha y hora de finalización: {DateTime.Now.ToString().ToUpperInvariant()}");
             Console.WriteLine($"Tiempo transcurrido: {tiempoTranscurridoString}");
 
-            Console.WriteLine($"Finalizado SequentialForEach(), fecha y hora de finalización: {DateTime.Now.ToString().ToUpperInvariant()}");
-
-            Console.WriteLine(Environment.NewLine);
+            Console.Write(Environment.NewLine);
         }
     }
 }
