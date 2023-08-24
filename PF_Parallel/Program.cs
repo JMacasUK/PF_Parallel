2000120a22cca49b36aabe3d8bdd3cc2e56c225b
JMacasUK <139647338+JMacasUK@users.noreply.github.com>
2023-08-23 20:19:11 -0700

    Refactor y simplificación de código

--- PF_Parallel/Program.cs
+++ PF_Parallel/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 
@@ -7,17 +8,18 @@ namespace PF_Parallel
 {
     internal class Program
     {
-        static string rutaEscanear = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        static string rutaEscanear = $"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}";
         static string[] archivos = Directory.GetFiles(rutaEscanear, "*.*", SearchOption.AllDirectories);
         static TimeSpan espacioDeTiempoParallel;
         static TimeSpan espacioDeTiempoSequential;
+
 
         static void Main(string[] args)
         {
             Console.WriteLine($"Se detectaron {archivos.Length} archivos en el directorio {rutaEscanear}");
             Console.WriteLine("");
 
-            ParallelForEach();
+            ParallelForEach(); 
 
             SequentialForEach();
 
@@ -33,15 +35,14 @@ namespace PF_Parallel
                 
                 FileInfo fileInfoArchivo = new FileInfo( rutaArchivo );
 
-                using (FileStream fileStream = fileInfoArchivo.Open(FileMode.Open))
+                using (FileStream fileStream = fileInfoArchivo.Open(FileMode.Open, FileAccess.Read))
                 {
                     try
                     {
                         fileStream.Position = 0;
                         byte[] hashValue = sha256.ComputeHash(fileStream);
 
-                        //Console.Write($"{fileInfoArchivo.Name}: ");
-                        //ImprimirByteArrayHexadecimal(hashValue);
+                        //Console.WriteLine($"{fileInfoArchivo.Name}: {GetByteArrayHexadecimal(hashValue)}");
                     }
                     catch (IOException ioException)
                     {
@@ -55,14 +56,16 @@ namespace PF_Parallel
             }
         }
 
-        public static void ImprimirByteArrayHexadecimal(byte[] byteArray)
+        public static string GetByteArrayHexadecimal(byte[] byteArray)
         {
+            string sha256text = string.Empty;
+
             for (int i = 0; i < byteArray.Length; i++)
             {
                 Console.Write($"{byteArray[i]:X2}".ToLowerInvariant());
             }
 
-            Console.WriteLine();
+            return sha256text;
         }
 
 
@@ -78,18 +81,15 @@ namespace PF_Parallel
             
             TimeSpan espacioDeTiempoParallel = cronometro.Elapsed;
             
-            string tiempoTranscurridoString = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                espacioDeTiempoParallel.Hours, espacioDeTiempoSequential.Minutes, espacioDeTiempoSequential.Seconds, espacioDeTiempoSequential.Milliseconds);
-            
             Console.WriteLine($"Finalizado ParallelForEach(), fecha y hora de finalización: {DateTime.Now:O}");
-            Console.WriteLine($"Tiempo transcurrido: {tiempoTranscurridoString}");
+            Console.WriteLine($"Tiempo transcurrido en parallel: {espacioDeTiempoParallel.TotalSeconds} segundos");
 
             Console.Write(Environment.NewLine);
         }
 
         public static void SequentialForEach()
         {
-            Console.WriteLine($"Ejecutando SequentialForEach(), fecha y hora de inicio: {DateTime.Now.ToString().ToUpperInvariant()}");
+            Console.WriteLine($"Ejecutando SequentialForEach(), fecha y hora de inicio: {DateTime.Now:O}");
 
             Stopwatch cronometro = new Stopwatch();
 
@@ -103,12 +103,9 @@ namespace PF_Parallel
             cronometro.Stop();
 
             TimeSpan espacioDeTiempoSequential = cronometro.Elapsed;
-
-            string tiempoTranscurridoString = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
-                espacioDeTiempoSequential.Hours, espacioDeTiempoSequential.Minutes, espacioDeTiempoSequential.Seconds, espacioDeTiempoSequential.Milliseconds);
             
-            Console.WriteLine($"Finalizado SequentialForEach(), fecha y hora de finalización: {DateTime.Now.ToString().ToUpperInvariant()}");
-            Console.WriteLine($"Tiempo transcurrido: {tiempoTranscurridoString}");
+            Console.WriteLine($"Finalizado SequentialForEach(), fecha y hora de finalización: {DateTime.Now:O}");
+            Console.WriteLine($"Tiempo transcurrido en secuencial: {espacioDeTiempoSequential.TotalSeconds} segundos");
 
             Console.Write(Environment.NewLine);
         }
